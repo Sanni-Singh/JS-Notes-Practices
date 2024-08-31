3556ddbfbd25f956c2624200897dcf5cf964b4a1
Sanni-Singh <aryansingh7371@gmail.com>
2024-08-31 17:33:09 +0530

    adding loops & array notes with an proper example

---
+++ Array/arrays.js
@@ -0,0 +1,61 @@
+// Declear a Array
+// const arr = [10 , 20 , 30 , 40];
+// console.log(arr);
+// ------------------------------------------------------
+
+// Arrays Method
+const arr = [20 , 10 , 30, 50 , 40];
+// console.log(arr.length); to calulate the length of the array
+// console.log(arr.toString()); to convert the number to the string
+// console.log(arr.at(2)); to acces the ele of the given index
+// console.log(arr.join("*")); to join the * in the give array 
+// arr.pop(); to delete the value of the last index 
+// arr.push(100); to add the value in the last index of the given arrray 
+// arr.shift(); to delete the first index value fro the array 
+// arr.unshift(55); to add the value at at the starting index of the array
+// delete arr[0]; to delete the val of that index 
+// const temp = [1 , 2, 3 , 4];
+// const newArray = arr.concat(temp) to concate the two array in a new aarray 
+// arr.copyWithin(3 , 0) to make the copy of value in the array 
+// const a = [[1 , 2] , [3 , 4], [ 5 , 6]];
+// const newArray = a.flat(); to add all the values in one array from the 2D array
+// arr.splice(2 , 0 , -100 , -200) is used to add the value in the arra with we have pased 
+// const sub = arr.slice(1);
+// console.log(sub); to delete the value with the help of the given index 
+// console.log(arr.indexOf(50)); to get the index of the given val 
+// console.log(arr.lastIndexOf(50)); to get the last index of the array
+// console.log(arr.includes(50)); to verify is the value is present in the array or not 
+// const temp = arr.find(myFunction);
+// function myFunction(value , index , array) {
+//     return value > 18;
+// }
+// console.log(temp); to cheak is there ant digit is greeter or not with the given value : this will return a value
+// const temp = arr.findIndex(myFunction);
+// function myFunction(value , index , array) {
+//     return value > 50;
+// }
+// console.log(temp); to cheak the value is greater or nt but there this e=will return a index
+
+// const temp = arr.findLast(myFunction);
+// function myFunction(value , index , array) {
+//     return value > 18;
+// }
+// console.log(temp); to acces the the greater value of the given value
+
+// const temp = arr.findLastIndex(myFunction);
+// function myFunction(value , index , array) {
+//     return value > 18;
+// }
+// console.log(temp); to acces the the greater value(acces the value) of the given value
+
+
+
+
+
+console.log(arr);
+
+
+
+
+
+
--- Loops/loops.js
+++ Loops/loops.js
@@ -1,4 +1,94 @@
-for(let i = 0; i < 10; i++){
-    console.log(i);
+//------------------------ loops(for sam as DSA)---------------------
+
+// for loop
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
     
-}
+// }
+// -----------------------------------------------------------------
+// while loop
+// let i = 0;
+// while(i <= 10){
+//     console.log(i ++);
+    
+// }
+// ----------------------------------------------------------------------------
+// for each loop
+// const arr = [10 , 20, 30 , 49];
+// arr.forEach( (val) => {
+//     console.log(val);
+    
+// })
+// -------------------------------------------------------------------------------------
+// for in loop 
+//const arr = [10 , 20, 30 , 49];
+// -----for index print---
+// for(let i in arr){
+//     console.log(arr[i]);
+    
+// }
+// -----for value print---
+// for(let i in arr){
+//     console.log(arr[i]);
+    
+// }
+// -----------------------------------------------------------------------------------
+// for of Loop
+// const arr = [10 , 20, 30 , 49];
+// for(let i of arr){
+//     console.log(i);
+    
+// }
+// ---------------------------------------------------------------------------------------
+// here i am iterating on string char
+// const str = "Sanni Singh"
+// for(let i of str){
+//     console.log(i);
+    
+// }
+// -------------------------------------------------------
+//  JS set ...crete & printing the set
+
+// const letter = new Set();
+// letter.add("S");
+// letter.add("a");
+// letter.add("n");
+// letter.add("n");
+// letter.add("i");
+// console.log(letter);
+
+// -------------------------------------------
+// travelling on set using for each loopp
+// const name = new Set(["s" , "a" , "n" ,"n" ,"i"]);
+// name.forEach( (char) => {
+//     console.log(char);
+    
+// })
+// -------------------------------------------
+
+// map--
+// creating the maap using hardcode values
+// const hm = ([
+//     ["Sanni" , 400],
+//     ["ram" , 4002],
+//     ["rahim" , 400],
+//     ["sahim" , 5400]
+// ]);
+// // hm.forEach( (val) {
+// //     console.log();
+    
+// // })
+// console.log(hm);
+
+
+
+// creating the maap using user values
+// const hm = new Map();
+// hm.set("sanni" , 299);
+// hm.set("pratik" , 29009);
+// hm.set("sanny" , 2299);
+// hm.set("natiki" , 2991);
+
+// console.log(hm);
+// ------------------------------------------------
+// filter
