f5da9b236ffa72413c4313c72be4f91bb563215d
Sanni-Singh <aryansingh7371@gmail.com>
2024-09-30 14:45:53 +0530

    learning DOM

---
+++ DOM/selectors/seelect.js
@@ -0,0 +1,13 @@
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+// ----------Id Selector----------------------
+// const val = document.getElementById('header');
+// const val1 = document.querySelectorAll('#main');
+// const val2 = document.querySelector('.header2');
+
+
+// const val2 = document.getElementsByClassName("header3")
+
+// const val2 = document.getElementsByTagName("h1")
+console.log(val2);
+
+
