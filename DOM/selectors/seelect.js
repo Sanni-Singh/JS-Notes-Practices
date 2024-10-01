356b0ef9b9992add20fe0520ef87a585b3e32cf1
Sanni-Singh <aryansingh7371@gmail.com>
2024-10-01 22:47:33 +0530

    adding some chnges

--- DOM/selectors/seelect.js
+++ DOM/selectors/seelect.js
@@ -8,6 +8,22 @@
 // const val2 = document.getElementsByClassName("header3")
 
 // const val2 = document.getElementsByTagName("h1")
-console.log(val2);
+// console.log(val2);
 
+// let flag = 1;
+// const a = document.querySelector(".box")
+// const b = document.getElementById("btn")
 
+// b.addEventListener('click', function(e){
+//     if( flag === 1){
+//         a.style.backgroundColor="yellow";
+//         flag = 0;
+//         b.style.cssText=(backgroundColor="white",color="black")
+//     }
+//     else {
+//         a.style.backgroundColor="white";
+//         flag = 1;
+//         b.style.backgroundColor="black"
+//     }
+// })
+const sb = document.querySelectorAll(".box")
